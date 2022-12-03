872e0bdd279c1bf1405ba862bdbe22232f215d23
Mathias Peters <mathias.peters6@gmail.com>
2022-12-03 19:35:59 +0100

    Solution for day 3

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -4,11 +4,38 @@ pub fn day3() {
 }
 
 pub fn part1() -> usize {
-    raw_input().len()
+    raw_input()
+        .lines()
+        .map(|line| (&line[..line.len() / 2], &line[line.len() / 2..]))
+        .map(|(fc, sc)| fc.chars().filter(|c| sc.contains(*c)).nth(0))
+        .map(|c| c.map(|c| char_to_priority(c)).unwrap_or(0))
+        .sum()
 }
 
 pub fn part2() -> usize {
-    raw_input().len()
+    let lines = raw_input().lines().collect::<Vec<_>>();
+    lines
+        .chunks(3)
+        .map(|group| {
+            group[0]
+                .chars()
+                .filter(|c| group[1].contains(*c) && group[2].contains(*c))
+                .nth(0)
+        })
+        .map(|c| c.map(|c| char_to_priority(c)).unwrap_or(0))
+        .sum()
+}
+
+fn char_to_priority(c: char) -> usize {
+    let val = c as u8;
+    let res = if val >= 97 {
+        val - 96
+    } else if val >= 65 {
+        val - 38
+    } else {
+        0
+    };
+    res as usize
 }
 
 #[cfg(not(test))]
@@ -27,11 +54,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(0, part1());
+        assert_eq!(157, part1());
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(0, part2());
+        assert_eq!(70, part2());
     }
 }
