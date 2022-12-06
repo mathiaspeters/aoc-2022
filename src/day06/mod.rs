e5c2e5c81b4ac34249708b99daaf3d782203e2dc
Mathias Peters <mathias.peters6@gmail.com>
2022-12-06 08:50:06 +0100

    Solution for day 6

--- src/day06/mod.rs
+++ src/day06/mod.rs
@@ -4,11 +4,34 @@ pub fn day6() {
 }
 
 pub fn part1() -> usize {
-    raw_input().len()
+    let input = raw_input().chars().collect::<Vec<_>>();
+    for i in 3..input.len() {
+        if all_different(&input[i - 3..=i]) {
+            return i + 1;
+        }
+    }
+    panic!("No result");
 }
 
 pub fn part2() -> usize {
-    raw_input().len()
+    let input = raw_input().chars().collect::<Vec<_>>();
+    for i in 13..input.len() {
+        if all_different(&input[i - 13..=i]) {
+            return i + 1;
+        }
+    }
+    panic!("No result");
+}
+
+fn all_different(s: &[char]) -> bool {
+    for i in 0..s.len() {
+        for j in i + 1..s.len() {
+            if s[i] == s[j] {
+                return false;
+            }
+        }
+    }
+    true
 }
 
 #[cfg(not(test))]
@@ -27,11 +50,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(0, part1());
+        assert_eq!(7, part1());
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(0, part2());
+        assert_eq!(19, part2());
     }
 }
